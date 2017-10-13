b90bcf7882b9bbfd95278e6808f5ec15cc4c4313
Ryan Record <rrecord@skysync.com>
2017-10-13 13:57:14 -0500

    sf-1318: --json true to --json

--- samples/powershell/basic-nfs-to-box-copy-job/skysync-cli-sample-delete-connections.ps1
+++ samples/powershell/basic-nfs-to-box-copy-job/skysync-cli-sample-delete-connections.ps1
@@ -1,8 +1,8 @@
-﻿$connections = skysync-cli connections list --json true | ConvertFrom-Json
+﻿$connections = skysync-cli connections list --json | ConvertFrom-Json
 
 #delete all connections except default 'My Computer' connection
 foreach ($conn in $connections | Where-Object {($_.name -ne 'My Computer') -and ($_.status -ne 'deleted')}) {
     skysync-cli connections delete $conn.id
 }
 
-skysync-cli connections list --json true | ConvertFrom-Json | Format-Table
+skysync-cli connections list --json | ConvertFrom-Json | Format-Table
--- samples/powershell/basic-nfs-to-box-copy-job/skysync-cli-sample-delete-jobs.ps1
+++ samples/powershell/basic-nfs-to-box-copy-job/skysync-cli-sample-delete-jobs.ps1
@@ -1,8 +1,8 @@
-﻿$jobs = skysync-cli jobs list --json true | ConvertFrom-Json 
+﻿$jobs = skysync-cli jobs list --json | ConvertFrom-Json 
 
 #delete all transfer jobs (preserve system jobs)
 foreach ($job in $jobs | Where-Object {($_.kind -eq 'transfer') -and ($_.status -ne 'deleted')}) {
     skysync-cli jobs delete $job.id
 }
 
-skysync-cli jobs list --json true | ConvertFrom-Json | Format-Table 
+skysync-cli jobs list --json | ConvertFrom-Json | Format-Table 
