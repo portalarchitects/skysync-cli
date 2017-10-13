f393214cbd5c602fde5a07ea1d479735abdabb18
rklind <rlind@skysync.com>
2017-10-13 13:36:17 -0600

    SF-1321: implement changing of passwords in the cli (#4)

---
+++ src/commands/users/password/change.ts
@@ -0,0 +1,18 @@
+import { runCommand } from '../../../util/command';
+import { outputFormatID } from '../util';
+const open = require('open')
+
+export = {
+	command: 'change <old-password> <new-password>',
+	desc: 'Change user\'s password',
+	handler: argv => {
+		runCommand(argv, async (client, output) => {
+			const user = await client.users.patch({
+					id: "me",
+					old_password: argv.oldPassword,
+					new_password: argv.newPassword
+			});
+			output.writeItem(user, outputFormatID);
+		});
+	}
+}
--- src/sdk/models/users.ts
+++ src/sdk/models/users.ts
@@ -5,9 +5,11 @@ import { Role } from './roles'
 export interface User extends IEntityIdentifier<string> {
 	login?: string;
 	password?: string;
+	old_password?: string;
+	new_password?: string;
 	name?: string;
 	email?: string;
 	disabled?: boolean;
 	group?: OwnershipGroup;
-	roles?: Role[];
-};
+	roles?: Role[];	
+}
