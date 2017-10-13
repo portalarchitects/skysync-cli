e4ef372041506e179cf17e06c6cc7f53ee659bdc
Ryan Record <rrecord@skysync.com>
2017-10-13 13:24:30 -0500

    sf-1318: delete old diretory

--- examples/skysync-cli-example-delete-connections.ps1
+++
@@ -1,8 +0,0 @@
-﻿$connections = skysync-cli connections list | ConvertFrom-Json
-
-#delete all connections except default 'My Computer' connection
-foreach ($conn in $connections | Where-Object {$_.name -ne 'My Computer'}) {
-    skysync-cli connections delete $conn.id
-}
-
-skysync-cli connections list | ConvertFrom-Json | Format-Table
