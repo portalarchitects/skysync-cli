9566e7d38a1ff670a2ca88ed8d3d68d8f4d90e54
Rebecca Lind <rlind@skysync.com>
2018-02-15 09:35:15 -0700

    SF-2018: Filter out undefined query parameters

--- src/sdk/http/http-client.ts
+++ src/sdk/http/http-client.ts
@@ -99,12 +99,22 @@ export abstract class HttpClient<TRequest, TResponse> implements IHttpClient {
 		}
 	}
 
+	static getFilteredParameters(params?: any): any {
+		let filteredParams = {};
+		for (const param in params) {
+			if (params[param] !== undefined) {
+				filteredParams[param] = params[param];
+			}
+		}
+		return filteredParams;
+	}
+
 	static getUrl(requestPath: string, baseUrl?: string, params?: any): string {
 		if (baseUrl && baseUrl.length > 0 && requestPath.indexOf('://') === -1) {
 			requestPath = baseUrl + requestPath;
 		}
 		if (params) {
-			requestPath = `${requestPath}?${qs.stringify(params)}`;
+			requestPath = `${requestPath}?${qs.stringify(HttpClient.getFilteredParameters(params))}`;
 		}
 		return requestPath;
 	}
