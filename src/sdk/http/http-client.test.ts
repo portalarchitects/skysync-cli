3850ef46234adebef3bc13a7aed2a099ce123a82
Rebecca Lind <rlind@skysync.com>
2018-02-15 10:24:11 -0700

    SF-2018: added tests

--- src/sdk/http/http-client.test.ts
+++ src/sdk/http/http-client.test.ts
@@ -15,5 +15,48 @@ describe('HttpClient', () => {
 			const url = HttpClient.getUrl('http://localhost:8000/v1/connections', 'http://localhost:9090/');
 			expect(url).to.eql('http://localhost:8000/v1/connections');
 		});
+
+		it('can get qualified url with single parameter', () => {
+			const params = {
+				foo: 'fooParameter'
+			}
+			const url = HttpClient.getUrl('http://localhost:8000/v1/connections', 'http://localhost:9090/', params);
+			expect(url).to.eql('http://localhost:8000/v1/connections?foo=fooParameter');
+		})
+
+		it('can get qualified url with multiple parameters', () => {
+			const params = {
+				foo: 'fooParameter',
+				bar: 'barParameter',
+				baz: 'bazParameter'
+			}
+			const url = HttpClient.getUrl('http://localhost:8000/v1/connections', 'http://localhost:9090/', params);
+			expect(url).to.eql('http://localhost:8000/v1/connections?foo=fooParameter&bar=barParameter&baz=bazParameter');
+		})
+		
+		it('can get qualified url with filtered parameters', () => {
+			const params = {
+				foo: undefined,
+				bar: 'barParameter',
+				baz: 'bazParameter'
+			}
+			const url = HttpClient.getUrl('http://localhost:8000/v1/connections', 'http://localhost:9090/', params);
+			expect(url).to.eql('http://localhost:8000/v1/connections?bar=barParameter&baz=bazParameter');
+		})
 	});
+	
+	describe('getFilteredParameters', () => {
+		it('should filter undefined parameters', () => {
+			const params = {
+				foo: undefined,
+				bar: 'barParameter',
+				baz: 'bazParameter'
+			}
+			const filteredParams = HttpClient.getFilteredParameters(params);
+			expect(filteredParams).to.eql({
+				bar: 'barParameter',
+				baz: 'bazParameter'
+			});
+		})
+	})	
 });
