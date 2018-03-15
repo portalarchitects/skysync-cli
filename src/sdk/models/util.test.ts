a3b552ec082d5ae734d1e91a212a0640681771b5
Shaun Becker <smbecker@gmail.com>
2018-03-15 17:08:08 -0400

    Reduce unit formatting precision and optimize

--- src/sdk/models/util.test.ts
+++ src/sdk/models/util.test.ts
@@ -3,72 +3,76 @@ import expect = require('expect.js');
 
 describe('formatBytes', () => {
 	it('can format positive bytes', () => {
-		expect(util.formatBytes(10)).to.eql('10 B');
-		expect(util.formatBytes(100)).to.eql('100 B');
+		expect(util.formatBytes(10)).to.eql('10\xa0B');
+		expect(util.formatBytes(100)).to.eql('100\xa0B');
 	});
 
 	it('can format positive kb', () => {
-		expect(util.formatBytes(Math.pow(2, 10))).to.eql('1.0 KB');
-		expect(util.formatBytes(Math.pow(2, 10) * 1.21)).to.eql('1.2 KB');
-		expect(util.formatBytes(Math.pow(2, 10) * 9.8)).to.eql('9.8 KB');
+		expect(util.formatBytes(Math.pow(2, 10))).to.eql('1.0\xa0KB');
+		expect(util.formatBytes(Math.pow(2, 10) * 1.21)).to.eql('1.2\xa0KB');
+		expect(util.formatBytes(Math.pow(2, 10) * 9.8)).to.eql('9.8\xa0KB');
 	});
 
 	it('can format positive mb', () => {
-		expect(util.formatBytes(Math.pow(2, 20))).to.eql('1.0 MB');
-		expect(util.formatBytes(Math.pow(2, 20) * 1.213)).to.eql('1.21 MB');
-		expect(util.formatBytes(Math.pow(2, 20) * 9.81)).to.eql('9.81 MB');
+		expect(util.formatBytes(Math.pow(2, 20))).to.eql('1.0\xa0MB');
+		expect(util.formatBytes(Math.pow(2, 20) * 1.213)).to.eql('1.2\xa0MB');
+		expect(util.formatBytes(Math.pow(2, 20) * 9.81)).to.eql('9.8\xa0MB');
 	});
 
 	it('can format positive gb', () => {
-		expect(util.formatBytes(Math.pow(2, 30))).to.eql('1.0 GB');
-		expect(util.formatBytes(Math.pow(2, 30) * 1.213)).to.eql('1.21 GB');
-		expect(util.formatBytes(Math.pow(2, 30) * 9.81)).to.eql('9.81 GB');
+		expect(util.formatBytes(Math.pow(2, 30))).to.eql('1.0\xa0GB');
+		expect(util.formatBytes(Math.pow(2, 30) * 1.213)).to.eql('1.2\xa0GB');
+		expect(util.formatBytes(Math.pow(2, 30) * 9.81)).to.eql('9.8\xa0GB');
 	});
 
 	it('can format positive tb', () => {
-		expect(util.formatBytes(Math.pow(2, 40))).to.eql('1.0 TB');
-		expect(util.formatBytes(Math.pow(2, 40) * 1.213)).to.eql('1.213 TB');
-		expect(util.formatBytes(Math.pow(2, 40) * 9.81)).to.eql('9.81 TB');
+		expect(util.formatBytes(Math.pow(2, 40))).to.eql('1.0\xa0TB');
+		expect(util.formatBytes(Math.pow(2, 40) * 1.213)).to.eql('1.2\xa0TB');
+		expect(util.formatBytes(Math.pow(2, 40) * 9.81)).to.eql('9.8\xa0TB');
 	});
 
 	it('can format large positive tb', () => {
-		expect(util.formatBytes(Math.pow(2, 40) * 1000)).to.eql('1,000.0 TB');
-		expect(util.formatBytes(Math.pow(2, 40) * 1.213 * 1000)).to.eql('1,213.0 TB');
-		expect(util.formatBytes(Math.pow(2, 40) * 9.81 * 1000)).to.eql('9,810.0 TB');
+		expect(util.formatBytes(Math.pow(2, 40) * 1024)).to.eql('1.0\xa0PB');
+		expect(util.formatBytes(Math.pow(2, 40) * 1.213 * 1024)).to.eql('1.2\xa0PB');
+		expect(util.formatBytes(Math.pow(2, 40) * 9.81 * 1024)).to.eql('9.8\xa0PB');
 	});
 
 	it('can format negative bytes', () => {
-		expect(util.formatBytes(-10)).to.eql('-10 B');
-		expect(util.formatBytes(-100)).to.eql('-100 B');
+		expect(util.formatBytes(-10)).to.eql('-10\xa0B');
+		expect(util.formatBytes(-100)).to.eql('-100\xa0B');
 	});
 
 	it('can format negative kb', () => {
-		expect(util.formatBytes(-Math.pow(2, 10))).to.eql('-1.0 KB');
-		expect(util.formatBytes(-Math.pow(2, 10) * 1.21)).to.eql('-1.2 KB');
-		expect(util.formatBytes(-Math.pow(2, 10) * 9.8)).to.eql('-9.8 KB');
+		expect(util.formatBytes(-Math.pow(2, 10))).to.eql('-1.0\xa0KB');
+		expect(util.formatBytes(-Math.pow(2, 10) * 1.21)).to.eql('-1.2\xa0KB');
+		expect(util.formatBytes(-Math.pow(2, 10) * 9.8)).to.eql('-9.8\xa0KB');
 	});
 
 	it('can format negative mb', () => {
-		expect(util.formatBytes(-Math.pow(2, 20))).to.eql('-1.0 MB');
-		expect(util.formatBytes(-Math.pow(2, 20) * 1.21)).to.eql('-1.21 MB');
-		expect(util.formatBytes(-Math.pow(2, 20) * 9.8)).to.eql('-9.8 MB');
+		expect(util.formatBytes(-Math.pow(2, 20))).to.eql('-1.0\xa0MB');
+		expect(util.formatBytes(-Math.pow(2, 20) * 1.21)).to.eql('-1.2\xa0MB');
+		expect(util.formatBytes(-Math.pow(2, 20) * 9.8)).to.eql('-9.8\xa0MB');
 	});
 
 	it('can format negative gb', () => {
-		expect(util.formatBytes(-Math.pow(2, 30))).to.eql('-1.0 GB');
-		expect(util.formatBytes(-Math.pow(2, 30) * 1.21)).to.eql('-1.21 GB');
-		expect(util.formatBytes(-Math.pow(2, 30) * 9.8)).to.eql('-9.8 GB');
+		expect(util.formatBytes(-Math.pow(2, 30))).to.eql('-1.0\xa0GB');
+		expect(util.formatBytes(-Math.pow(2, 30) * 1.21)).to.eql('-1.2\xa0GB');
+		expect(util.formatBytes(-Math.pow(2, 30) * 9.8)).to.eql('-9.8\xa0GB');
 	});
 
 	it('can format negative tb', () => {
-		expect(util.formatBytes(-Math.pow(2, 40))).to.eql('-1.0 TB');
-		expect(util.formatBytes(-Math.pow(2, 40) * 1.21)).to.eql('-1.21 TB');
-		expect(util.formatBytes(-Math.pow(2, 40) * 9.8)).to.eql('-9.8 TB');
+		expect(util.formatBytes(-Math.pow(2, 40))).to.eql('-1.0\xa0TB');
+		expect(util.formatBytes(-Math.pow(2, 40) * 1.21)).to.eql('-1.2\xa0TB');
+		expect(util.formatBytes(-Math.pow(2, 40) * 9.8)).to.eql('-9.8\xa0TB');
 	});
 
 	it('can format large negative tb', () => {
-		expect(util.formatBytes(-Math.pow(2, 40) * 1000)).to.eql('-1,000.0 TB');
-		expect(util.formatBytes(-Math.pow(2, 40) * 1.213 * 1000)).to.eql('-1,213.0 TB');
-		expect(util.formatBytes(-Math.pow(2, 40) * 9.81 * 1000)).to.eql('-9,810.0 TB');
+		expect(util.formatBytes(-Math.pow(2, 40) * 1024)).to.eql('-1.0\xa0PB');
+		expect(util.formatBytes(-Math.pow(2, 40) * 1.213 * 1024)).to.eql('-1.2\xa0PB');
+		expect(util.formatBytes(-Math.pow(2, 40) * 9.81 * 1024)).to.eql('-9.8\xa0PB');
+	});
+
+	it('can format large yb', () => {
+		expect(util.formatBytes(Math.pow(2, 80) * 1024)).to.eql('1,024.0\xa0YB');
 	});
 });
