76f1908fdcd5c67e66d2ff4d159d29b9cc665e30
Shaun Becker <smbecker@gmail.com>
2018-03-16 11:38:22 -0400

    Reduce unit formatting precision and optimize (#34)

--- src/sdk/models/util.ts
+++ src/sdk/models/util.ts
@@ -1,34 +1,42 @@
-const unitsAndFormatDigit = [
-	['B', 0],
-	['KB', 1],
-	['MB', 2],
-	['GB', 2],
-	['TB', 3]
-];
-const maxByteDigit = unitsAndFormatDigit.length - 1;
+const units = [ 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB' ];
+const maxExponent = units.length - 1;
 const unitSize = 1024;
+const unitBase = Math.log(unitSize);
+
+const noDigit = new Intl.NumberFormat('en', {
+	style: 'decimal',
+	useGrouping: true,
+	minimumFractionDigits: 0,
+	maximumFractionDigits: 0
+});
+
+const withDigit = new Intl.NumberFormat('en', {
+	style: 'decimal',
+	useGrouping: true,
+	minimumFractionDigits: 1,
+	maximumFractionDigits: 1
+});
 
 const formatBytesImpl = (num: number): string => {
 	const negative = num < 0;
-	num = Math.abs(num);
-
-	let index = 0;
-	while (num >= unitSize) {
-		num = num / unitSize;
-		index++;
-		if (index === maxByteDigit) {
-			break;
-		}
+	if (negative) {
+		num = -num;
 	}
 
-	const [abbreviation, digit] = unitsAndFormatDigit[index];
-	const byteFormatter = new Intl.NumberFormat('en', {
-		style: 'decimal',
-		useGrouping: true,
-		minimumFractionDigits: Math.min(<number>digit, 1),
-		maximumFractionDigits: <number>digit
-	});
-	return `${negative ? '-' : ''}${byteFormatter.format(num)} ${abbreviation}`;
+	let e = Math.floor(Math.log(num) / unitBase);
+	if (e < 0) {
+		e = 0;
+	} else if (e > maxExponent) {
+		e = maxExponent;
+	}
+
+	if (e > 0) {
+		num = num / Math.pow(2, e * 10);
+	}
+
+	const abbreviation = units[e];
+	const formatter = e === 0 ? noDigit : withDigit;
+	return `${negative ? '-' : ''}${formatter.format(num)}\xa0${abbreviation}`;
 }
 
 export namespace util {
