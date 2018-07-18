1e55022d363d1907bd601052d043f83f27fe7d7e
Rebecca Lind <rlind@skysync.com>
2018-07-18 13:56:22 -0600

    sf-2333a: update implementation to transfer folders

--- src/sdk/resources/transferFolders.ts
+++ src/sdk/resources/transferFolders.ts
@@ -4,12 +4,6 @@ import { TransferItem } from '../models/transfers';
 
 export class TransferFoldersResource extends PagedResource<TransferItem> {
 	constructor(httpClient: IHttpClient) {
-		super(httpClient, 'item', null, 'item');
-	}
-	
-	async list(params?: any): Promise<TransferItem[]> {
-		this.resourcePath = `transfers/${params.id}/folders`;
-		delete params.id;
-		return super.list(params);
+		super(httpClient, 'item', null, 'item', null, 'transfers/folders');
 	}
 }
