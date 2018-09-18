4d090a58a0f11da9ba89814d461260ce2bf41611
Kris Peterson <kpeterson@portalarchitects.com>
2018-09-18 11:21:12 -0600

    Refactored TransferSecurityMappingResource to extend Resource

--- src/sdk/resources/transferSecurityMappings.ts
+++ src/sdk/resources/transferSecurityMappings.ts
@@ -1,14 +1,14 @@
 import { IHttpClient } from '../http';
-import { BaseResource, getPagedResponse, getTypedResponse, PagedResult } from './resource';
+import { getPagedResponse, getTypedResponse, Resource, PagedResult } from './resource';
 import { TransferSecurityMapping } from '../models';
 
-export class TransferSecurityMappingsResource extends BaseResource {
+export class TransferSecurityMappingsResource extends Resource<TransferSecurityMapping> {
 	constructor(httpClient: IHttpClient) {
-		super(httpClient);
+		super(httpClient, 'item', 'item', null, null, 'transfers');
 	}
 
 	async page(params?: any): Promise<PagedResult<TransferSecurityMapping>> {
-		const result = await this.httpClient.get(`transfers/${params && params.id}/security_map`, this.mergeDefaultParams(params));
+		const result = await this.httpClient.get(`${this.resourcePath}/${params && params.id}/security_map`, this.mergeDefaultParams(params));
 		const items = getTypedResponse<TransferSecurityMapping[]>(result, 'item');
 		return getPagedResponse<TransferSecurityMapping>(result, items);
 	}
