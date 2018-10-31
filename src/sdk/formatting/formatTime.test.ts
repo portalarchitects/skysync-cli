cee3f1c29c70140054000b2cdfc674be08ef7f3d
Jon Tirjan <jtirjan@skysync.com>
2018-10-31 18:16:40 -0500

    Revert temp test values

--- src/sdk/formatting/formatTime.test.ts
+++ src/sdk/formatting/formatTime.test.ts
@@ -10,11 +10,11 @@ describe('formatTime', () => {
 
 	it('can display timezone name standard', () => {
 		const date = '2016-01-10 4:34:50';
-		expect(formatTime(appendTimeZoneCode(date), {showTimeZone: true})).to.eql(`04:34 AM ${getTimeZoneCode(new Date(date))}`);
+		expect(formatTime(appendTimeZoneCode(date), {showTimeZone: true})).to.eql(`4:34 AM ${getTimeZoneCode(new Date(date))}`);
 	});
 
 	it('can display timezone name dst', () => {
 		const date = '2016-07-10 13:34:50';
-		expect(formatTime(appendTimeZoneCode(date), {showTimeZone: true})).to.eql(`01:34 PM ${getTimeZoneCode(new Date(date))}`);
+		expect(formatTime(appendTimeZoneCode(date), {showTimeZone: true})).to.eql(`1:34 PM ${getTimeZoneCode(new Date(date))}`);
 	});
 });
