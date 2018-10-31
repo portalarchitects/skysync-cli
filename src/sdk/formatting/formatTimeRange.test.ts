9bb236594e4e59bbfe53760a1cbc4504132c7dde
Jon Tirjan <jtirjan@skysync.com>
2018-10-31 17:53:40 -0500

    SF-3148: Allow TimeZone to be passed into tests

--- src/sdk/formatting/formatTimeRange.test.ts
+++ src/sdk/formatting/formatTimeRange.test.ts
@@ -1,25 +1,25 @@
 import { formatTimeRange } from './formatTimeRange';
 import expect = require('expect.js');
-import {timeZoneCode} from './testUtil';
+import {appendTimeZoneCode} from './testUtil';
 
 describe('formatTimeRange', () => {
 	it('can format time ranges', () => {
-		expect(formatTimeRange(`2016-04-10 4:34:50${timeZoneCode}`, `2016-04-10 13:34:50${timeZoneCode}`)).to.eql('4:34 AM-1:34 PM');
+		expect(formatTimeRange(appendTimeZoneCode('2016-04-10 4:34:50'), appendTimeZoneCode('2016-04-10 13:34:50'))).to.eql('4:34 AM-1:34 PM');
 	});
 
 	it('should trim suffix when both produce same suffix', () => {
-		expect(formatTimeRange(`2016-04-10 4:34:50${timeZoneCode}`, `2016-04-10 8:34:50${timeZoneCode}`)).to.eql('4:34-8:34 AM');
-		expect(formatTimeRange(`2016-04-10 14:34:50${timeZoneCode}`, `2016-04-10 18:34:50${timeZoneCode}`)).to.eql('2:34-6:34 PM');
+		expect(formatTimeRange(appendTimeZoneCode('2016-04-10 4:34:50'), appendTimeZoneCode('2016-04-10 8:34:50'))).to.eql('4:34-8:34 AM');
+		expect(formatTimeRange(appendTimeZoneCode('2016-04-10 14:34:50'), appendTimeZoneCode('2016-04-10 18:34:50'))).to.eql('2:34-6:34 PM');
 	});
 
 	it('should return `start` without separator when no `end`', () => {
-		expect(formatTimeRange(`2016-04-10 4:34:50${timeZoneCode}`, null)).to.eql('4:34 AM');
-		expect(formatTimeRange(`2016-04-10 14:34:50${timeZoneCode}`, undefined)).to.eql('2:34 PM');
+		expect(formatTimeRange(appendTimeZoneCode('2016-04-10 4:34:50'), null)).to.eql('4:34 AM');
+		expect(formatTimeRange(appendTimeZoneCode('2016-04-10 14:34:50'), undefined)).to.eql('2:34 PM');
 	});
 
 	it('should return `end` without separator when no `start`', () => {
-		expect(formatTimeRange(null, `2016-04-10 8:34:50${timeZoneCode}`)).to.eql('8:34 AM');
-		expect(formatTimeRange(undefined, `2016-04-10 18:34:50${timeZoneCode}`)).to.eql('6:34 PM');
+		expect(formatTimeRange(null, appendTimeZoneCode('2016-04-10 8:34:50'))).to.eql('8:34 AM');
+		expect(formatTimeRange(undefined, appendTimeZoneCode('2016-04-10 18:34:50'))).to.eql('6:34 PM');
 	});
 
 	it('should return null when no `start` or `end`', () => {
--- src/sdk/formatting/getDateFormat.ts
+++ src/sdk/formatting/getDateFormat.ts
@@ -1,5 +1,4 @@
 const isInTest = typeof global['it'] === 'function';
-
-const timeZone = isInTest && { timeZone: 'America/Phoenix' };
+const timeZone = isInTest && { timeZone: process.env.TIME_ZONE || 'UTC' };
 
 export const getDateFormat = args => new Intl.DateTimeFormat('en', Object.assign(args, timeZone));
