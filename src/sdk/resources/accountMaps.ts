c103e1d0169d6dbf07ea15e4af290848c5c63e78
Alex Volinsky <alex.volinsky@gmail.com>
2018-12-17 16:55:15 -0500

    Added location parameter to user/group map exclusions (#86)
    * Added location parameter to user/group map exclusions
    
    * Code review

--- src/sdk/resources/accountMaps.ts
+++ src/sdk/resources/accountMaps.ts
@@ -13,7 +13,7 @@ export class AccountMapsResource extends PagedResource<AccountMap> {
 		return new AccountMapExceptionsResource(this.httpClient, accountMapId);
 	}
 	
-	exclusions(accountMapId: string): AccountMapExclusionsResource {
-		return new AccountMapExclusionsResource(this.httpClient, accountMapId);
+	exclusions(accountMapId: string, location?: 'source' | 'destination'): AccountMapExclusionsResource {
+		return new AccountMapExclusionsResource(this.httpClient, accountMapId, location);
 	}
 }
