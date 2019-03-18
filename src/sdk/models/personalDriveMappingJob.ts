887752473b5d892979f1f8c9c1ecbda82614ef3d
Alex Volinsky <alex.volinsky@gmail.com>
2019-03-18 14:14:48 -0400

    Added more convention model properties

--- src/sdk/models/personalDriveMappingJob.ts
+++ src/sdk/models/personalDriveMappingJob.ts
@@ -1,4 +1,4 @@
-import { TransferJob } from "./transfers";
+import { TransferJob } from './transfers';
 
 export enum PersonalDriveMappingJobConventionMatch {
 	Account = 'account',
@@ -6,9 +6,35 @@ export enum PersonalDriveMappingJobConventionMatch {
 	Ldap = 'ldap'
 }
 
+export enum PersonalDriveMappingJobConventionUnmappedPolicy {
+	Ignore = 'ignore',
+	WarnAndSkip = 'warn',
+	Provision = 'add'
+}
+
+export enum PersonalDriveMappingJobConventionUsers {
+	Source = 'source',
+	Destination = 'destination',
+	Ldap = 'ldap'
+}
+
 export interface PersonalDriveMappingConvention {
 	match?: PersonalDriveMappingJobConventionMatch;
 	include_matches?: boolean;
+	remove_invalid?: boolean;
+	unmapped_policy?: PersonalDriveMappingJobConventionUnmappedPolicy;
+	users?: PersonalDriveMappingJobConventionUsers;
+	search_domain?: string;
+	map_by?: {
+		caption?: boolean;
+		default?: boolean;
+		email?: boolean;
+		fuzzy?: boolean;
+		id?: boolean;
+		ldap?: boolean;
+		name?: boolean;
+		username?: boolean;
+	};
 }
 
 export interface PersonalDriveMappingJob extends TransferJob {
