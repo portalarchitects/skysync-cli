b18eb43072d82d7dae082879edf8510922b3af7b
Jon Tirjan <Jon@Tirjan.local>
2019-06-14 17:53:49 -0500

    SF-3864: Add multi-item actions

--- src/sdk/resources/transferItems.ts
+++ src/sdk/resources/transferItems.ts
@@ -16,11 +16,23 @@ export class TransferItemsResource extends PagedResource<TransferItem> {
 		return this.modifyItem(itemid, {retry: true}, token);
 	}
 
+	ignoreMultiple(itemids?: any, token?: CancellationToken): Promise<void> {
+		return this.modifyMultiple(itemids, {ignore: true}, token);
+	}
+
+	retryMultiple(itemids?: any, token?: CancellationToken): Promise<void> {
+		return this.modifyMultiple(itemids, {retry: true}, token);
+	}
+	
 	private async modifyItem(itemid: number, params: any, token?: CancellationToken): Promise<void> {
 		await this.httpClient.patch(`${this.resourcePath}/${itemid}`, undefined, this.mergeDefaultParams(params), token);
 	}
 
-	downloadCsv(params?: any, token?: CancellationToken): Promise<string> {
+	private async modifyMultiple(itemids: any, params: any, token?: CancellationToken): Promise<void> {
+		await this.httpClient.patch(this.resourcePath, undefined, this.mergeParams(itemids, params), token);
+	}
+	
+		downloadCsv(params?: any, token?: CancellationToken): Promise<string> {
 		return this.httpClient.get(`${this.resourcePath}.csv`, params, token);
 	}
 }
