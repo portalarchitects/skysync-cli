570b854d29b66abd7152352ee009492337024a07
Rebecca Lind <rlind@skysync.com>
2020-06-09 14:03:30 -0600

    VNEXT-110: Add policy locaitons

--- src/sdk/resources/policyLocations.ts
+++ src/sdk/resources/policyLocations.ts
@@ -1,15 +1,9 @@
 import { PolicyLocation } from '../models';
 import { IHttpClient } from '../http';
-import { getTypedResponse, BaseResource } from './resource';
-import { CancellationToken } from '../cancellation-token';
+import { PagedResource } from './resource';
 
-export class PolicyLocationsResource extends BaseResource {
+export class PolicyLocationsResource extends PagedResource<PolicyLocation> {
 	constructor(httpClient: IHttpClient) {
-		super(httpClient);
-	}
-
-	async get(id: string, params?: any, token?: CancellationToken): Promise<PolicyLocation> {
-		const result = await this.httpClient.get(`policies/${id}/locations`, this.mergeDefaultParams(params), token);
-		return getTypedResponse<PolicyLocation>(result);
+		super(httpClient, 'location', null, 'location', null, 'policies/locations');
 	}
 }
