f1c3e0582e891203102f6d1fc41e8ee2cbdda1e7
Rebecca Lind <rlind@skysync.com>
2020-06-12 08:14:03 -0600

    VNEXT-110: update policy statistics

--- src/sdk/models/policyStatistics.ts
+++ src/sdk/models/policyStatistics.ts
@@ -27,9 +27,10 @@ export type PolicyJobStatisticsTimeline = {
 export interface PolicyJobStatistics extends IEntityIdentifier<string> {
 	timeline?: PolicyJobStatisticsTimeline;
 	by_risk?: PolicyStatisticsList;
+	by_group?: PolicyStatisticsList;
+	by_activity?: PolicyStatisticsList;
 	by_location_health?: PolicyStatisticsList;
 	by_flagged_category?: PolicyStatisticsList;
-	by_activity?: PolicyStatisticsList;
 	count?: number;
 	locations?: number;
 	executions?: number;
