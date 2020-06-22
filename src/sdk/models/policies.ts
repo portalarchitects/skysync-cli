cd13dfdc6f272b3634ded5be1ef95f5358edae5e
Rebecca Lind <rlind@skysync.com>
2020-06-22 14:59:42 -0600

    VNEXT-110: code review feedback

--- src/sdk/models/policies.ts
+++ src/sdk/models/policies.ts
@@ -1,47 +1,18 @@
 import { IEntityIdentifier, IAuditedEntity } from './base';
-import { TimeInterval } from './time';
-import { JobScheduler } from './scheduler';
 import { JobCategory } from './jobCategories';
-import { JobExecutionStatus, JobSchedule} from './jobs';
-import { StoragePlatform } from './connections';
-
-export enum PolicyLocationHealth {
-	Healthy = 'healthy',
-	Failing = 'failing',
-	Inactive = 'inactive',
-}
+import { Job } from './jobs';
+import { TransferPath } from './transfers';
 
 export interface Policy extends IEntityIdentifier<string>, IAuditedEntity {
-	kind?: string;
 	name?: string;
 	description?: string;
 	category?: JobCategory;
-	scheduler?: JobScheduler;
-	schedule?: JobSchedule;
-	status?: string;
-	execute_on?: number;
-	previous_execution: PolicyExecution;
-	execution: PolicyExecution;
 	disabled?: boolean;
 }
 
-export interface PolicyExecution {
-	policy_id?: string;
-	progress?: number;
-	phase?: string;
-	id?: number;
-	start_time?: number;
-	end_time?: number;
-	duration?: TimeInterval;
-	status?: JobExecutionStatus;
-	node_address?: string;
+export interface PolicyLocation extends Job {
+	policy?: PolicyLocationOptions;
 }
 
-export interface PolicyLocation extends IEntityIdentifier<string> {
-	name?: string;
-	platform?: StoragePlatform;
-	successes?: number;
-	failures?: number;
-	runs?: number;
-	health?: PolicyLocationHealth;
+export interface PolicyLocationOptions extends TransferPath {
 }
