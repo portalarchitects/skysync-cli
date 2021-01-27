4f7bcf0697a65bd8db2a681c0e8829b3a78945e7
mmitoff <mmitoff@skysync.com>
2021-01-27 10:08:27 -0500

    Creating separate enum for function names

--- src/sdk/models/metadataFilters.ts
+++ src/sdk/models/metadataFilters.ts
@@ -5,37 +5,40 @@ export enum MetadataConjunctionOperator {
 
 export enum MetadataPropertyFilterOperator {
 	eq = 'Equals',
-	Equals = 'Equals',
 	ne = 'Not Equal',
-	NotEqual = 'Not Equal',
 	lt = 'Less Than',
-	LessThan = 'Less Than',
 	le = 'Less Than Equal To',
-	LessThanEqualTo = 'Less Than Equal To',
 	gt = 'Greater Than',
-	GreaterThan = 'Greater Than',
 	ge = 'Greater Than Equal To',
-	GreaterThanEqualTo = 'Greater Than Equal To',
 	starts_with = 'Starts With',
-	StartsWith = 'Starts With',
 	not_starts_with = 'Does Not Start With',
-	DoesNotStartWith = 'Does Not Start With',
 	ends_with = 'Ends With',
-	EndsWith = 'Ends With',
 	not_ends_with = 'Does Not End With',
-	DoesNotEndWith = 'Does Not End With',
 	contains = 'Contains',
-	Contains = 'Contains',
 	not_contains = 'Does Not Contain',
-	DoesNotContain = 'Does Not Contain',
 	in = 'In',
-	In = 'In',
 	not_in = 'Not In',
-	NotIn = 'Not In',
 	empty = 'Empty',
-	Empty = 'Empty',
 	not_empty = 'Not Empty',
-	NotEmpty = 'Not Empty'
+}
+
+export enum MetadataFunctionName {
+	Now = 'Now',
+	Year = 'Year',
+	Month = 'Month',
+	Day = 'Day',
+	AddDays = 'Add Days',
+	AddMonths = 'Add Months',
+	AddYears = 'Add Years',
+	FormatDate = 'Format Date',
+	Concat = 'Concat',
+	Convert = 'Convert',
+	Contains = 'Contains',
+	StartsWith = 'Starts With',
+	EndsWith = 'Ends With',
+	GetExtension = 'Get Extension',
+	GetFileName = 'Get File Name',
+	GetFileNameWithoutExtension = 'Get File Name Without Extension'
 }
 
 export type MetadataComponentFilter = MetadataConjunctionFilter | MetadataPropertyFilter;
@@ -65,7 +68,7 @@ export interface MetadataCalculatedExpression {
 }
 
 export interface MetadataFunctionExpression {
-	function: string;
+	function: MetadataFunctionName | string;
 	args?: MetadataComponentExpression[];
 }
 
