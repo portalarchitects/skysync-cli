37661527945476d9e0540e98e54f45f9dfef72f3
Alex Volinsky <alex.volinsky@gmail.com>
2021-02-10 14:57:41 -0500

    Added file import for exclusions

--- src/sdk/resources/accountMapExclusions.ts
+++ src/sdk/resources/accountMapExclusions.ts
@@ -1,9 +1,14 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
 import { AccountMapException } from '../models/accountMaps';
+import { CancellationToken } from '../cancellation-token';
 
 export class AccountMapExclusionsResource extends PagedResource<AccountMapException> {
 	constructor(httpClient: IHttpClient, accountMapId: string, location?: 'source' | 'destination') {
 		super(httpClient, 'item', 'item', 'item', 'item', `account_maps/${accountMapId}/exclusions${location ? `/${location}` : ''}`);
+	}
+
+	import(body: FormData, params?: any, token?: CancellationToken): Promise<boolean> {
+		return this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
