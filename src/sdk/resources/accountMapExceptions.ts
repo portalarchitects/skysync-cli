9aac86f5c9305d178c06f9607335ce3869fa4410
Alex Volinsky <alex.volinsky@gmail.com>
2021-02-10 09:09:11 -0500

    Added account/group exception import methods (#162)

--- src/sdk/resources/accountMapExceptions.ts
+++ src/sdk/resources/accountMapExceptions.ts
@@ -1,9 +1,14 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
 import { AccountMapException } from '../models/accountMaps';
+import { CancellationToken } from '../cancellation-token';
 
 export class AccountMapExceptionsResource extends PagedResource<AccountMapException> {
 	constructor(httpClient: IHttpClient, accountMapId: string) {
 		super(httpClient, 'item', 'item', 'item', 'item', `account_maps/${accountMapId}/exceptions`);
+	}
+
+	import(body: FormData, params?: any, token?: CancellationToken): Promise<boolean> {
+		return this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
--- src/sdk/resources/groupMapExceptions.ts
+++ src/sdk/resources/groupMapExceptions.ts
@@ -1,9 +1,14 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
 import { GroupMapException } from '../models/groupMaps';
+import { CancellationToken } from '../cancellation-token';
 
 export class GroupMapExceptionsResource extends PagedResource<GroupMapException> {
 	constructor(httpClient: IHttpClient, groupMapId: string) {
 		super(httpClient, 'item', 'item', 'item', 'item', `Group_maps/${groupMapId}/exceptions`);
+	}
+
+	import(body: FormData, params?: any, token?: CancellationToken): Promise<boolean> {
+		return this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
