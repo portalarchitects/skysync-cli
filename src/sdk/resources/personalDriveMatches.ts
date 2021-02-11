9fb1c5671f385fcc413f1ed4f88fc0d52a1b6942
Alex Volinsky <alex.volinsky@gmail.com>
2021-02-11 14:42:30 -0500

    Added file import to personal_drives exceptions and matches

--- src/sdk/resources/personalDriveMatches.ts
+++ src/sdk/resources/personalDriveMatches.ts
@@ -1,8 +1,13 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
+import { CancellationToken } from '../cancellation-token';
 
 export class PersonalDriveMatchesResource extends PagedResource<Account> {
 	constructor(httpClient: IHttpClient, jobId: string) {
 		super(httpClient, 'item', 'item', 'item', 'item', `transfers/${jobId}/personal_drives/matches`);
+	}
+
+	import(body: FormData, params?: any, token?: CancellationToken): Promise<boolean> {
+		return this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
