3947e973d35420c1be388661c807fde8edac95f3
Alex Volinsky <alex.volinsky@gmail.com>
2021-02-11 09:20:57 -0500

    Added file import for exclusions (#163)

--- src/sdk/resources/groupMapExclusions.ts
+++ src/sdk/resources/groupMapExclusions.ts
@@ -1,9 +1,14 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
 import { GroupMapException } from '../models/groupMaps';
+import { CancellationToken } from '../cancellation-token';
 
 export class GroupMapExclusionsResource extends PagedResource<GroupMapException> {
 	constructor(httpClient: IHttpClient, groupMapId: string, location?: 'source' | 'destination') {
 		super(httpClient, 'item', 'item', 'item', 'item', `Group_maps/${groupMapId}/exclusions${location ? `/${location}` : ''}`);
+	}
+
+	import(body: FormData, params?: any, token?: CancellationToken): Promise<boolean> {
+		return this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
