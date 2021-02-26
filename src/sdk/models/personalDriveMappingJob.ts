1b4538c9be08e7fc36646e58921298ae3f46999d
Ben Tatum <btatum@skysync.com>
2021-02-26 09:29:01 -0500

    SF-8245 | remove map_by and unmapped_policy from PersonalDriveMappingConvention interface + delete personalDriveExceptions.ts

--- src/sdk/models/personalDriveMappingJob.ts
+++ src/sdk/models/personalDriveMappingJob.ts
@@ -7,12 +7,6 @@ export enum PersonalDriveMappingJobConventionMatch {
 	Ldap = 'ldap'
 }
 
-export enum PersonalDriveMappingJobConventionUnmappedPolicy {
-	Ignore = 'ignore',
-	WarnAndSkip = 'warn',
-	Provision = 'add'
-}
-
 export enum PersonalDriveMappingJobConventionUsers {
 	Source = 'source',
 	Destination = 'destination',
@@ -24,19 +18,8 @@ export interface PersonalDriveMappingConvention {
 	match?: PersonalDriveMappingJobConventionMatch;
 	include_matches?: boolean;
 	remove_invalid?: boolean;
-	unmapped_policy?: PersonalDriveMappingJobConventionUnmappedPolicy;
 	users?: PersonalDriveMappingJobConventionUsers;
 	search_domain?: string;
-	map_by?: {
-		caption?: boolean;
-		default?: boolean;
-		email?: boolean;
-		fuzzy?: boolean;
-		id?: boolean;
-		ldap?: boolean;
-		name?: boolean;
-		username?: boolean;
-	};
 }
 
 export interface PersonalDriveMappingJob extends TransferJob {
