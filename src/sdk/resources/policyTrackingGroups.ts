5b199ca49ce0a89cc18a938642dc460418146ad5
Jon Tirjan <JTirjan@skysync.com>
2021-05-27 18:36:23 -0500

    ACT-1163: Add PolicyTrackingGroup Resource

---
+++ src/sdk/resources/policyTrackingGroups.ts
@@ -0,0 +1,10 @@
+import { IHttpClient } from '../http';
+import { PagedResource}  from './resource';
+import { PolicyTrackingGroup } from '../models';
+
+export class PoliciesResource extends PagedResource<PolicyTrackingGroup> {
+	constructor(httpClient: IHttpClient, policyId?: number) {
+		super(httpClient, 'tracking_group');
+			this.resourcePath = policyId ? `policies/${policyId}/groups` : 'policies/groups';
+	}
+}
