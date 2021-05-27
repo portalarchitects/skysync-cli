c41e13d2c986051bcae4e3e7996787572d86930e
Jon Tirjan <JTirjan@skysync.com>
2021-05-27 18:40:05 -0500

    lint fix

--- src/sdk/resources/policyTrackingGroups.ts
+++ src/sdk/resources/policyTrackingGroups.ts
@@ -5,6 +5,6 @@ import { PolicyTrackingGroup } from '../models';
 export class PoliciesResource extends PagedResource<PolicyTrackingGroup> {
 	constructor(httpClient: IHttpClient, policyId?: number) {
 		super(httpClient, 'tracking_group');
-			this.resourcePath = policyId ? `policies/${policyId}/groups` : 'policies/groups';
+		this.resourcePath = policyId ? `policies/${policyId}/groups` : 'policies/groups';
 	}
 }
