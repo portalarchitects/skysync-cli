899cfb7ca79c940e2bb5344dcf2929e6650abded
Jon Tirjan <JTirjan@skysync.com>
2021-05-27 19:06:58 -0500

    Rename & Export

--- src/sdk/resources/policyTrackingGroups.ts
+++ src/sdk/resources/policyTrackingGroups.ts
@@ -2,7 +2,7 @@ import { IHttpClient } from '../http';
 import { PagedResource}  from './resource';
 import { PolicyTrackingGroup } from '../models';
 
-export class PoliciesResource extends PagedResource<PolicyTrackingGroup> {
+export class PolicyTrackingGroupsResource extends PagedResource<PolicyTrackingGroup> {
 	constructor(httpClient: IHttpClient, policyId?: number) {
 		super(httpClient, 'tracking_group');
 		this.resourcePath = policyId ? `policies/${policyId}/groups` : 'policies/groups';
