bf8c42f59af2fbce0589d7504d3aee9b6415b484
Jeffrey Rimko <jrimko@skysync.com>
2021-06-11 12:40:39 -0400

    ACT-1164: Added clone function.

--- src/sdk/resources/policyTrackingGroups.ts
+++ src/sdk/resources/policyTrackingGroups.ts
@@ -1,10 +1,16 @@
 import { IHttpClient } from '../http';
 import { PagedResource}  from './resource';
 import { PolicyTrackingGroup } from '../models';
+import { CancellationToken } from '../cancellation-token';
 
 export class PolicyTrackingGroupsResource extends PagedResource<PolicyTrackingGroup> {
 	constructor(httpClient: IHttpClient, policyId: string) {
 		super(httpClient, 'tracking_group');
 		this.resourcePath = `policies/${policyId}/tracking_groups`;
+	}
+
+	async clone(id: string, body?: any, params?: any, token?: CancellationToken): Promise<PolicyTrackingGroup> {
+		const group = await this.httpClient.post(`${this.resourcePath}/${id}/clone`, body, params, token);
+		return this.getSingle(group);
 	}
 }
