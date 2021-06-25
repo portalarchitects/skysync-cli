353e3cb3d74a8f673206514377ea634ba54018f2
Jim Partin <jpartin@skysync.com>
2021-06-24 22:48:47 -0400

    Add file import method for keywords

--- src/sdk/resources/entityTypeDictionaryElements.ts
+++ src/sdk/resources/entityTypeDictionaryElements.ts
@@ -1,9 +1,14 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
 import { EntityTypeDictionaryElement } from '../models';
+import { CancellationToken } from '../cancellation-token';
 
 export class EntityTypeDictionaryElementsResource extends PagedResource<EntityTypeDictionaryElement> {
 	constructor(httpClient: IHttpClient, dictionaryId: string) {
 		super(httpClient, 'dictionary_element', 'dictionary_element', 'dictionary_elements', 'dictionary_elements', `entity_types/dictionaries/${dictionaryId}/words`);
+	}
+
+	async import(body: FormData, params?: any, token?: CancellationToken): Promise<void> {
+		await this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
