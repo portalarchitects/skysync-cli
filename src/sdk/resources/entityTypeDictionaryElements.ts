86a45d2951eb5fdc994c36987c2049ec501b3e7b
jpartin2 <67276055+jpartin2@users.noreply.github.com>
2021-06-28 13:44:43 -0400

    ACT-1392: Add CSV file import method for keywords (#192)

--- src/sdk/resources/entityTypeDictionaryElements.ts
+++ src/sdk/resources/entityTypeDictionaryElements.ts
@@ -1,9 +1,14 @@
 import { IHttpClient } from '../http';
 import { PagedResource } from './resource';
 import { EntityTypeDictionaryElement } from '../models';
+import { CancellationToken } from '../cancellation-token';
 
 export class EntityTypeDictionaryElementsResource extends PagedResource<EntityTypeDictionaryElement> {
 	constructor(httpClient: IHttpClient, dictionaryId: string) {
 		super(httpClient, 'dictionary_element', 'dictionary_element', 'dictionary_elements', 'dictionary_elements', `entity_types/dictionaries/${dictionaryId}/words`);
+	}
+
+	import(body: FormData, params?: any, token?: CancellationToken): Promise<void> {
+		return this.httpClient.upload(this.resourcePath, null, body, params, token);
 	}
 }
