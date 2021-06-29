47043658102a96ea071adf282a76cb614ff931f0
Jeffrey Rimko <jrimko@skysync.com>
2021-06-29 13:18:28 -0400

    Update src/sdk/resources/policyActionTypes.ts
    Co-authored-by: jtirjan <jtirjan@skysync.com>

--- src/sdk/resources/policyActionTypes.ts
+++ src/sdk/resources/policyActionTypes.ts
@@ -1,8 +1,8 @@
 import { IHttpClient } from '../http';
-import { PagedResource } from './resource';
+import { Resource } from './resource';
 import { PolicyActionDescriptor } from '../models';
 
-export class PolicyActionTypesResource extends PagedResource<PolicyActionDescriptor> {
+export class PolicyActionTypesResource extends Resource<PolicyActionDescriptor> {
 	constructor(httpClient: IHttpClient) {
 		super(httpClient, 'policy_action_type', 'policy_action_type', 'policy_action_types', 'policy_action_types', 'policies/action_types');
 	}
