bf24e49b53ce4d43a628c15439bd87dddc70c7bc
Ben Tatum <btatum@skysync.com>
2021-08-04 15:02:11 -0400

    ACT-1098 | using Resource instead of PagedResource

--- src/sdk/resources/policyStatisticsSummary.ts
+++ src/sdk/resources/policyStatisticsSummary.ts
@@ -1,8 +1,8 @@
 import { PoliciesStatisticsSummary } from '../models';
 import { IHttpClient } from '../http';
-import { PagedResource } from './resource';
+import { Resource } from './resource';
 
-export class PoliciesStatisticsSummaryResource extends PagedResource<PoliciesStatisticsSummary> {
+export class PoliciesStatisticsSummaryResource extends Resource<PoliciesStatisticsSummary> {
 	constructor(httpClient: IHttpClient) {
 		const name = 'policies_stats_summary';
 		super(httpClient, name, null, name, null, 'policies/stats/summary');
