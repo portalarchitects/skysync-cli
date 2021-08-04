12cf963e74f4ddcf7b9826a4bfc35f6670781598
mmitoff <mmitoff@skysync.com>
2021-08-04 14:36:19 -0400

    Adding optional configuration prop to Extension model

--- src/sdk/models/extensions.ts
+++ src/sdk/models/extensions.ts
@@ -1,7 +1,9 @@
+import { PromptAttribute, PromptAttributes } from '.';
 import { IEntityIdentifier } from './base';
 
 export interface Extension extends IEntityIdentifier<string> {
 	description?: string;
 	version?: string;
 	disabled?: boolean;
+	configuration?: PromptAttribute[] | PromptAttributes;
 }
