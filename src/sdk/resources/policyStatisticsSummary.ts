f460ffed551fdc2462c03a5ecb13738f257a3902
Ben Tatum <btatum@skysync.com>
2021-08-04 08:19:34 -0400

    ACT-1098 | rm get func

--- src/sdk/resources/policyStatisticsSummary.ts
+++ src/sdk/resources/policyStatisticsSummary.ts
@@ -1,16 +1,10 @@
 import { PoliciesStatisticsSummary } from '../models';
 import { IHttpClient } from '../http';
-import { getTypedResponse, PagedResource } from './resource';
-import { CancellationToken } from '../cancellation-token';
+import { PagedResource } from './resource';
 
 export class PolicyStatisticsSummaryResource extends PagedResource<PoliciesStatisticsSummary> {
 	constructor(httpClient: IHttpClient) {
 		const name = 'policy_statistics_summary';
 		super(httpClient, name, null, name, null, 'policies/stats/summary');
-	}
-
-	async get(params?: any, token?: CancellationToken): Promise<PoliciesStatisticsSummary> {
-		const result = await this.httpClient.get(this.resourcePath, this.mergeDefaultParams(params), token);
-		return getTypedResponse<PoliciesStatisticsSummary>(result);
 	}
 }
