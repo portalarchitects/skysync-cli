4345aade2a0e4eca7a15dd7f433b98336a7e33da
jpartin2 <67276055+jpartin2@users.noreply.github.com>
2021-10-01 19:52:28 -0400

    Fix singular names for action types resource. (#228)

--- src/sdk/resources/policyActionTypes.ts
+++ src/sdk/resources/policyActionTypes.ts
@@ -4,6 +4,6 @@ import { PolicyActionDescriptor } from '../models';
 
 export class PolicyActionTypesResource extends Resource<PolicyActionDescriptor> {
 	constructor(httpClient: IHttpClient) {
-		super(httpClient, 'policy_action_type', 'policy_action_type', 'policy_action_types', 'policy_action_types', 'policies/action_types');
+		super(httpClient, 'policy_action_type', 'policy_action_types', 'policy_action_type', 'policy_action_types', 'policies/action_types');
 	}
 }
