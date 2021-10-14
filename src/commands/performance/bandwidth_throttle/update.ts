a2353a341459a61329910c670dc2e049fa0ec64c
alanconnor <aconnor@skysync.com>
2021-10-14 13:53:46 -0500

    SF-7794 | added ability to update from a json file

--- src/commands/performance/bandwidth_throttle/update.ts
+++ src/commands/performance/bandwidth_throttle/update.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 import { runCommand } from '../../../util/command';
 import { writeConfiguration } from './util';
 import { Performance } from '../../../sdk';
@@ -12,13 +13,23 @@ export = {
 				desc: 'The throttle options for uploads.',
 				type: 'string'
 			},
+			'upload-file': {
+				alias: 'u-file',
+				desc: 'The options JSON file',
+				type: 'string'
+			},
 			'download': {
 				alias: 'd',
 				desc: 'The throttle options for downloads.',
 				type: 'string'
+			},
+			'download-file': {
+				alias: 'd-file',
+				desc: 'The options JSON file',
+				type: 'string'
 			}
 		}).check(argv => {
-			if (argv.hasOwnProperty('upload') || argv.hasOwnProperty('download')) {
+			if (argv.hasOwnProperty('upload') || argv.hasOwnProperty('upload-file') || argv.hasOwnProperty('download') || argv.hasOwnProperty('download-file')) {
 				return true;
 			}
 			return 'Must include at least one option.';
@@ -26,13 +37,19 @@ export = {
 	},
 	handler: argv => {
 		runCommand(argv, async (client, output) => {
-			const performance: Performance = {};
-			if (argv.hasOwnProperty('upload')) {
-				performance.upload = JSON.parse(argv.upload);
+			let performance: Performance = {};
+			if (argv.hasOwnProperty('upload') || argv.hasOwnProperty('upload-file')) {
+				performance.upload = argv.upload ? JSON.parse(argv.upload) : undefined;
+				if (!performance.upload && argv.uploadFile) {
+					performance.upload = JSON.parse(fs.readFileSync(argv.uploadFile, 'utf-8'));
+				}
 			}
-			if (argv.hasOwnProperty('download')) {
-				performance.download = JSON.parse(argv.download);
-			}	
+			if (argv.hasOwnProperty('download') || argv.hasOwnProperty('download-file')) {
+				performance.download = argv.download ? JSON.parse(argv.download) : undefined;
+				if (!performance.download && argv.downloadFile) {
+					performance.download = JSON.parse(fs.readFileSync(argv.downloadFile, 'utf-8'));
+				}
+			}		
 			writeConfiguration(await client.performance.post(performance), output);
 		});
 	}
