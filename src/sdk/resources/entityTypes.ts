611dad5225ab9b4e4f93cfc7fda4d2b7355059dd
Michele Buselli <mbuselli@skysync.com>
2022-01-07 15:36:02 -0600

    Add clone

--- src/sdk/resources/entityTypes.ts
+++ src/sdk/resources/entityTypes.ts
@@ -27,4 +27,9 @@ export class EntityTypesResource extends PagedResource<EntityType | BlockListEnt
 		const result = await this.httpClient.upload(`${this.resourcePath}/test`, body, params, token);
 		return getTypedResponse<EntityTypeEvaluationResult[]>(result);
 	}
+
+	async clone(id: string, body: EntityType, params?: any, token?: CancellationToken): Promise<EntityType> {
+		const result = await this.httpClient.post(`${this.resourcePath}/${id}/clone`, body, params, token);
+		return this.getSingle(result);
+	}
 }
