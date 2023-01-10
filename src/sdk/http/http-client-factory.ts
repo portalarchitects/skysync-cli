6460cc739b20b10b1f48fad971650fab3e851091
Alex Murtha <amurtha@skysync.com>
2023-01-10 14:10:50 -0600

    SF-10011: Code review feedback

--- src/sdk/http/http-client-factory.ts
+++ src/sdk/http/http-client-factory.ts
@@ -5,19 +5,22 @@ declare const window: any;
 declare const WorkerGlobalScope: any;
 declare const global: any;
 
-function getFetch(): {fetch: FetchApi; formDataType: any } {
+function getFetch(): {fetch: FetchApi; formDataType: any; usingNodeFetch: boolean } {
 	return (function(g: any) {
 		let f = g?.fetch;
 		let formDataType = g?.FormData;
+		let usingNodeFetch = !Boolean(g?.window);
 		if (f) {
 			f = f.bind(g);
 		} else {
 			f = module.require('node-fetch');
 			formDataType = module.require('form-data');
+			usingNodeFetch = true;
 		}
 		return {
 			fetch: f,
-			formDataType
+			formDataType,
+			usingNodeFetch
 		};
 	})((typeof window !== 'undefined'
 		? window
@@ -28,6 +31,11 @@ function getFetch(): {fetch: FetchApi; formDataType: any } {
 }
 
 export function createHttpClient(baseAddress?: string, token?: IAuthorizationToken, site?: string): IHttpClient {
-	const { fetch, formDataType } = getFetch();
-	return new FetchHttpClient(fetch, formDataType, baseAddress || 'http://localhost:9090/', token, site);
+	const { fetch, formDataType, usingNodeFetch } = getFetch();
+	if (!usingNodeFetch) {
+		baseAddress = '/';
+	} else {
+		baseAddress = baseAddress || 'http://localhost:9090/';
+	}
+	return new FetchHttpClient(fetch, formDataType, baseAddress, token, site);
 }
