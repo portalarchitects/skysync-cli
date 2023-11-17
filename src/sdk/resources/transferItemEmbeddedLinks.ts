ab46bd58f7609ceb2b8b7921d5180f3c401ee855
Alex Murtha <amurtha@skysync.com>
2023-11-17 11:59:25 -0600

    ACT-5776: Added embedded link resource methods for updating status

--- src/sdk/resources/transferItemEmbeddedLinks.ts
+++ src/sdk/resources/transferItemEmbeddedLinks.ts
@@ -11,4 +11,24 @@ export class TransferItemEmbeddedLinksResource extends PagedResource<TransferIte
 	downloadCsv(params?: any, token?: CancellationToken): Promise<string> {
 		return this.httpClient.get(`${this.resourcePath}.csv`, params, token);
 	}
+    
+	ignoreAllLinksForItem(itemId: number, token?: CancellationToken): Promise<void> {
+		return this.modifyAllLinksForMultipleItems([itemId], {ignore: true}, token);
+	}
+
+	retryAllLinksForItem(itemId: number, token?: CancellationToken): Promise<void> {
+		return this.modifyAllLinksForMultipleItems([itemId], {retry: true}, token);
+	}
+
+	ignoreAllLinksForMultipleItems(itemIds: number[], params?: any, token?: CancellationToken): Promise<void> {
+		return this.modifyAllLinksForMultipleItems(itemIds, this.mergeParams(params, {ignore: true}), token);
+	}
+
+	retryAllLinksForMultipleItems(itemIds: number[], params?: any, token?: CancellationToken): Promise<void> {
+		return this.modifyAllLinksForMultipleItems(itemIds, this.mergeParams(params, {retry: true}), token);
+	}
+
+	private async modifyAllLinksForMultipleItems(itemIds: number[], params: any, token?: CancellationToken): Promise<void> {
+		await this.httpClient.patch(this.resourcePath, undefined, this.mergeParams(params, {items: itemIds}), token);
+	}
 }
